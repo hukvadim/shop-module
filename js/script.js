131ff2cde827fab69c79621c1667e6ff681798e9
Vadim <hukvadim@gmail.com>
2023-11-18 12:34:53 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+import viewCart from "./app/cart.js";
+import viewCategory from "./app/сategory.js";
+import viewProducts from "./app/products.js";
+import viewHotOffers from "./app/hotOffers.js";
+import searchProducts from "./app/search.js";
+
+// Виводимо список товарів
+viewProducts();
+viewCategory();
+viewHotOffers();
+searchProducts();
+viewCart();
+
